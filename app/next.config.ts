edd3005d9f75893b90e858f1701344e54c320a94
Steve Jonk <stevejonk1993@gmail.com>
2026-09-07 14:53:02 +0200

    fix: reduce sentry

--- app/next.config.ts
+++ app/next.config.ts
@@ -35,8 +35,9 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+  // Off: widenClientFileUpload roughly doubles source-map upload size/memory during build,
+  // which was crashing the build server (OOM). Re-enable once the server has more headroom.
+  widenClientFileUpload: false,
 
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
